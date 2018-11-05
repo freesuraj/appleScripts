c4f6c6b4b8f60d4b3df892e55a90b9cdad10ea3f
Suraj Pathak <freesuraj@gmail.com>
2018-11-06 09:11:27 +1100

    update eeeeh script

--- Eeeeh/viber.sh
+++ Eeeeh/viber.sh
@@ -11,12 +11,17 @@ SWEAR[1]=Bhutrooo
 SWEAR[2]=Yetroooo
 SWEAR[3]=Ghantaaa
 SWEAR[4]=Vejaaaaa
+SWEAR[5]=Gedaaaa
+SWEAR[6]=La*oooo
+SWEAR[7]=Mujiiiii
+SWEAR[8]=Mulaaaaa
+SWEAR[9]=Ghwaaaaak
 
 randSwear=$[$RANDOM % ${#SWEAR[@]}]
 swear=${SWEAR[$randSwear]}
 
-caption="${swear}\n ${1}"
+caption="${swear}\n ${1} \n(#Eeehattack)"
 randCol=$[$RANDOM % ${#COLOR[@]}]
 color=${COLOR[$randCol]}
-/usr/local/bin/convert -background $color  -fill white  -pointsize 80 -size 400x400 -gravity Center  caption:"${caption}" $HOME/Desktop/test.jpeg 
+/usr/local/bin/convert -background $color  -fill white  -pointsize 60 -size 400x400 -gravity Center  caption:"${caption}" $HOME/Desktop/test.jpeg 
 /usr/local/bin/copyq write image/jpeg - < $HOME/Desktop/test.jpeg && /usr/local/bin/copyq select 0
